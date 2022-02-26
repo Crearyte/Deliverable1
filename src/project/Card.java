50a23c37bf589e9afc86f27e14dc9355ffaa068d
TerrelC <crearyte@sheridanc.on.ca>
2022-02-26 11:07:03 -0500

    implemented Safa's CARD.java code

--- src/project/Card.java
+++ src/project/Card.java
@@ -1,25 +1,100 @@
 /**
  * SYST 17796 Project Base code.
  * Students can modify and extend to implement their game.
- * Add your name as an author and the date!
+ * Safa
  */
 package project;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-/**
- * A class to be used as the base Card class for the project. Must be general enough to be instantiated for any Card
- * game. Students wishing to add to the code should remember to add themselves as a modifier.
- *
- * @author dancye
- */
-public abstract class Card {
-    //default modifier for child classes
+public class Card implements Comparable<Card>
+{
+  private Card()
+  {
+  }
 
-    /**
-     * Students should implement this method for their specific children classes
-     *
-     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
-     */
-    @Override
-    public abstract String toString();
+  public enum CARDNUMBER
+  {
+TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10), JACK(11), QUEEN(12), KING(13), ACE(
+  14);
 
+  private int   ord;
+
+  private CARDNUMBER(int i)
+  {
+  this.ord = i;
+  }
+
+  /**
+* Returns the ordinal position of the enum
+*
+* @return int ord
+*/
+  public int getOrd()
+  {
+  return ord;
+  }
+  }
+
+  public enum CARDTYPE
+  {
+CLUB, DIAMOND, HEARTS, SPADE;
+  }
+
+  private CARDNUMBER cdNumber;
+  private CARDTYPE cdType;
+
+  public CARDNUMBER getCdNumber()
+  {
+  return cdNumber;
+  }
+
+  public CARDTYPE getCdType()
+  {
+  return cdType;
+  }
+
+  public static List<Card> getPackOfCards()
+  {
+List<Card> crdLst = new ArrayList<Card>();
+
+  for (CARDTYPE types : CARDTYPE.values())
+  {
+  for (CARDNUMBER cNums : CARDNUMBER.values())
+  {
+Card cd = new Card();
+cd.cdNumber = cNums;
+cd.cdType = types;
+crdLst.add(cd);
+  }
+  }
+  return crdLst;
+  }
+
+  public static void shuffleCards(List<Card> cards)
+  {
+  Collections.shuffle(cards);
+  }
+
+@Override
+  public int compareTo(Card o)
+  {
+  if (this.getCdNumber() == o.getCdNumber())
+  {
+  return 0;
+  }
+  else if (this.getCdNumber().getOrd() > o.getCdNumber().getOrd())
+  {
+  return 1;
+  }
+  else
+  return -1;
+  }
+
+@Override
+  public String toString()
+  {
+  return "CARD [cdNumber=" + cdNumber + ", cdType=" + cdType + "]";
+  }
 }
