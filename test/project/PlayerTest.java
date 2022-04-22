49c10ea8b736fb138b3219bf3561b395d65548fb
Hyemin <98358420+lHyeminl@users.noreply.github.com>
2022-04-22 17:37:57 -0400

    Update PlayerTest.java
    Modify Good/Bad/Boundary

--- test/project/PlayerTest.java
+++ test/project/PlayerTest.java
@@ -5,144 +5,103 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-
 public class PlayerTest {
-    
+
     public PlayerTest() {
     }
-    
+
     @Before
     public void setUp() {
     }
 
-    /**
-     * Test of getPlayerId method, of class Player.
-     */
     @Test
-    public void testGetPlayerId() {
-        System.out.println("getPlayerId");
-        Player instance = null;
-        int expResult = 0;
-        int result = instance.getPlayerId();
+    public void testHashCodeGood() {
+        System.out.println("Good hashCode");
+        boolean expResult = true;
+        if (Player.class.hashCode() > 31) {
+            boolean result = true;
+            assertEquals(expResult, result);
+        }
+
+    }
+
+    @Test
+    public void testHashCodeBad() {
+        System.out.println("Bad hashCode");
+        boolean expResult = true;
+        if (Player.class.hashCode() < 31) {
+            boolean result = false;
+            assertEquals(expResult, result);
+        }
+    }
+
+    @Test
+    public void testHashCodeBoundary() {
+        System.out.println("Boundary hashCode");
+        boolean expResult = true;
+        if (Player.class.hashCode() == 31) {
+            boolean result = false;
+            assertEquals(expResult, result);
+        }
+    }
+
+    @Test
+    public void testEqualsGood() {
+        System.out.println("Good equals");
+        Object obj1 = true;
+        Object obj2 = true;
+        boolean expResult = true;
+        boolean result = obj1.equals(obj2);
         assertEquals(expResult, result);
     }
 
-    /**
-     * Test of setPlayerId method, of class Player.
-     */
     @Test
-    public void testSetPlayerId() {
-        System.out.println("setPlayerId");
-        int playerId = 0;
-        Player instance = null;
-        instance.setPlayerId(playerId);
-    }
-
-    /**
-     * Test of getPlayerName method, of class Player.
-     */
-    @Test
-    public void testGetPlayerName() {
-        System.out.println("getPlayerName");
-        Player instance = null;
-        String expResult = "";
-        String result = instance.getPlayerName();
-        assertEquals(expResult, result);
-    }
-
-    /**
-     * Test of setPlayerName method, of class Player.
-     */
-    @Test
-    public void testSetPlayerName() {
-        System.out.println("setPlayerName");
-        String playerName = "";
-        Player instance = null;
-        instance.setPlayerName(playerName);
-    }
-
-    /**
-     * Test of getPoints method, of class Player.
-     */
-    @Test
-    public void testGetPoints() {
-        System.out.println("getPoints");
-        Player instance = null;
-        int expResult = 0;
-        int result = instance.getPoints();
-        assertEquals(expResult, result);
-    }
-
-    /**
-     * Test of setPoints method, of class Player.
-     */
-    @Test
-    public void testSetPoints() {
-        System.out.println("setPoints");
-        int points = 0;
-        Player instance = null;
-        instance.setPoints(points);
-    }
-
-    /**
-     * Test of getResult method, of class Player.
-     */
-    @Test
-    public void testGetResult() {
-        System.out.println("getResult");
-        Player instance = null;
-        String expResult = "";
-        String result = instance.getResult();
-        assertEquals(expResult, result);
-    }
-
-    /**
-     * Test of setResult method, of class Player.
-     */
-    @Test
-    public void testSetResult() {
-        System.out.println("setResult");
-        String result_2 = "";
-        Player instance = null;
-        instance.setResult(result_2);
-    }
-
-    /**
-     * Test of hashCode method, of class Player.
-     */
-    @Test
-    public void testHashCode() {
-        System.out.println("hashCode");
-        Player instance = null;
-        int expResult = 0;
-        int result = instance.hashCode();
-        assertEquals(expResult, result);
-    }
-
-    /**
-     * Test of equals method, of class Player.
-     */
-    @Test
-    public void testEquals() {
-        System.out.println("equals");
-        Object obj = null;
-        Player instance = null;
+    public void testEqualsBad() {
+        System.out.println("Bad equals");
+        Object obj1 = true;
+        Object obj2 = false;
         boolean expResult = false;
-        boolean result = instance.equals(obj);
+        boolean result = obj1.equals(obj2);
         assertEquals(expResult, result);
     }
 
-    /**
-     * Test of compareTo method, of class Player.
-     */
     @Test
-    public void testCompareTo() {
-        System.out.println("compareTo");
-        Player o = null;
-        Player instance = null;
-        int expResult = 0;
-        int result = instance.compareTo(o);
+    public void testEqualsBoundary() {
+        System.out.println("Boundary equals");
+        Object obj1 = true;
+        Object obj2 = true;
+        boolean expResult = true;
+        boolean result = obj1.equals(obj2);
         assertEquals(expResult, result);
     }
-    
+
+    @Test
+    public void testCompareToGood() {
+        System.out.println("Good compareTo");
+        Player p1 = new Player(4);
+        Player p2 = new Player(2);
+        int expResult = 0;
+        int result = p1.compareTo(p2);
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testCompareToBad() {
+        System.out.println("Bad compareTo");
+        Player p1 = new Player(2);
+        Player p2 = new Player(4);
+        int expResult = 0;
+        int result = p1.compareTo(p2);
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testCompareToBoundary() {
+        System.out.println("compareTo");
+        Player p1 = new Player(3);
+        Player p2 = new Player(3);
+        int expResult = 0;
+        int result = p1.compareTo(p2);
+        assertEquals(expResult, result);
+    }
 }
